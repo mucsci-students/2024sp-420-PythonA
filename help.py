c3464ae7159268f17da3b68cc772f2f93298f22a
Peter F <frdmanp@gmail.com>
2024-02-01 22:08:54 -0500

    Help Menu
    Help menu is done. testHelp is a bit of a messy dump of info, but does
    verify that the functions are working as intended and the error is
    raised correctly, which I think is the important part for now.

--- help.py
+++ help.py
@@ -1,23 +1,23 @@
 '''Application help menu, to be called when user asks for help.'''
 
-from CustomExceptions import CommandNotFoundError   #if an invalid command was entered after help
+from CustomExceptions import CustomExceptions   #if an invalid command was entered after help
 
 def basicHelp():
     menu = "Use 'help [command]' for detailed information about command usage\n\t"
     menu += "class - displays all commands related to class creation and modification\n\t"
-    menu += "att - displays all commands related to attribute creation and modification\n\t"
-    menu += "rel - displays all comands related to relationship creation and modification\n\t"
-    menu += "list - displays all commands related to list creation and modification\n\t"
-    menu += "save - displays usage for the save command\n\t"
-    menu += "load - displays usage for the load command\n"
+    menu += "att   - displays all commands related to attribute creation and modification\n\t"
+    menu += "rel   - displays all comands related to relationship creation and modification\n\t"
+    menu += "list  - displays all commands related to list creation and modification\n\t"
+    menu += "save  - displays usage for the save command\n\t"
+    menu += "load  - displays usage for the load command\n"
 
     return menu
 
 def cmdHelp(command:str):
-    menu = CommandNotFoundError(command)
+    menu = CustomExceptions.CommandNotFoundError(command)
 
     if("class" == command):
-        menu = "-a name - adds a class with name 'name'. Cannot add classes with duplicate or invalid names\n"
+        menu = "-a name  - adds a class with name 'name'. Cannot add classes with duplicate or invalid names\n"
         menu += "-d name - deletes a class with name 'name'\n"
         menu += "-r old new - renames class 'old' to 'new'. Cannot rename classes to duplicate or invalid names\n"
         menu += "-s name - selects the class with name 'name' as the one to be modified\n"
@@ -34,15 +34,15 @@ def cmdHelp(command:str):
         menu += "-d dest - deletes a relationship between the selected class and destination 'dest' if one exists\n"
     
     elif("list" == command):
-        menu = "all - list all classes and their contents in the UML Diagram\n"
+        menu = "all    - list all classes and their contents in the UML Diagram\n"
         menu += "class - list all classes in the UML Diagram\n"
-        menu += "rel - list all relationships in the UML Diagram\n"
+        menu += "rel   - list all relationships in the UML Diagram\n"
         menu += "class name - list all contents of class 'name'\n"
-        menu += "rel name - list all relationships of class 'name'\n"
+        menu += "rel name   - list all relationships of class 'name'\n"
     
     elif("save" == command):
         menu = "-n name - saves the UML Diagram as a JSON file with name 'name'\n"
-        menu += "just save will save the UML Diagram to its existing file\n"
+        menu += "typing save on its own will save the UML Diagram to its existing file\n"
     
     elif("load" == command):
         menu = "name - loads the file with name 'name.json' if one exists\n"
