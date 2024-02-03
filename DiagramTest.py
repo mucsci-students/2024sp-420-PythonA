959f582721dd39006861716036e419fc3589c91b
timbmoser <150080066+timbmoser@users.noreply.github.com>
2024-02-03 17:11:18 -0500

    Added Relation Tests, Fixed a typo in addClass Testing.

--- DiagramTest.py
+++ DiagramTest.py
@@ -15,7 +15,7 @@ def main():
     addTest = Test("addEntity", dia.addEntity)
     print(addTest.exec("Valid Name", None, "Entity1"))
     print(addTest.exec("Invalid Name", "Name must contain only alphanumeric characters", "Entity 1"))
-    print(addTest.exec("Existing Name", "Entity with name 'Entity1' already exists.", "Entity1"))
+    print(addTest.exec("Existing Name", "Object with name 'Entity1' already exists.", "Entity1")) #TYPO FIXED
 
     # renameClass Testing
     renameTest = Test("renameEntity", dia.renameEntity)
@@ -26,5 +26,17 @@ def main():
     # listEntities Testing
     listTest = Test("listEntities", dia.listEntities)
     print(listTest.exec("Entity exists", "NewEntity"))
+    
+    # add_relation Testing
+    dia.addEntity("Entity2") #Need second to test relation
+    add_relation_test = Test("add_relation", dia.add_relation)
+    print(add_relation_test.exec("Valid input", "Success.", "Entity1", "Entity2"))
+    print(add_relation_test.exec("Relation already exists.", "Relation already exists.", "Entity1", "Entity2"))
+    
+    # delete_relation Testing
+    delete_relation_test = Test("delete_relation", dia.delete_relation)
+    print(delete_relation_test.exec("Valid input.", "Success.", "Entity1", "Entity2"))
+    print(delete_relation_test.exec("No relation.", "Relation does not exist.", "Entity1", "Entity2"))
+    
     
 main()
