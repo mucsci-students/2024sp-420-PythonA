eb1767e3a94df66d4bf9c27f37d08f4e4a82dbed
timbmoser <150080066+timbmoser@users.noreply.github.com>
2024-02-03 21:26:43 -0500

    Comments to spec

--- Relation.py
+++ Relation.py
@@ -4,24 +4,61 @@ class Relation:
     def __init__(self, source, destination):
         """
         Creates a relation from source and destination.
+        
+        Args:
+            source (Entity): The entity at the start of the relation.
+            destination (Entity): The entity at the end of the relation.
+            
+        Raises:
+            None.
+            
+        Returns:
+            None.
         """
         self._source = source
         self._destination = destination
-        
+    
     def get_source(self):
         """
-        Returns the source.
+        Returns the source entity of the the relation.
+        
+        Args:
+            None.
+            
+        Raises:
+            None.
+        
+        Returns:
+            source (Entity): The source entity of the relation.
         """
         return self._source
     
     def get_destination(self):
         """
-        Returns the destination.
+        Returns the destination entity of the relation.
+        
+        Args:
+            None.
+            
+        Raises:
+            None.
+            
+        Returns:
+            destination (Entity): The destination entity of the relation.
         """
         return self._destination
 
     def __str__(self):
         """
         Returns a string representation of a relation.
+        
+        Args:
+            None.
+            
+        Raises:
+            None.
+            
+        Returns:
+            A string representation of the relation.
         """
         return f'{self._source} -> {self._destination}'
