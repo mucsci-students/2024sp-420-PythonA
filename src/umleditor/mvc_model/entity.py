976ae0c3774ef61f32f258b5d4c80e6530e2b53a
Marshall Feng <fengdier2@126.com>
2024-03-01 15:09:50 -0500

    Fix issues of add_method_and_params taking any # of params

--- src/umleditor/mvc_model/entity.py
+++ src/umleditor/mvc_model/entity.py
@@ -132,7 +132,7 @@ class Entity:
             new_method = UML_Method(method_name)
             self._methods.append(new_method)
     
-    def add_method_and_params(self, method_name: str, params:list[str] = []) :
+    def add_method_and_params(self, method_name: str, *params) :
         """
         Adds a method with specified parameters to the class.
 
@@ -143,10 +143,7 @@ class Entity:
         """
         print("WE ARE HERE")
         self.add_method(method_name)
-        # Don't add params if none passed
-        if not params:
-            return
-        #self.get_method(method_name).add_parameters(params)
+        self.get_method(method_name).add_parameters([params])
 
     def delete_method(self, method_name: str):
         """
