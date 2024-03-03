b64a01425d06bdaef04b4dbc9e75d05a2b8b2b37
Marshall Feng <fengdier2@126.com>
2024-03-03 16:45:32 -0500

    Add more tests for serializer

--- src/test/test_serializer.py
+++ src/test/test_serializer.py
@@ -145,3 +145,49 @@ def test_methods():
     assert 'Param1' in load_dia.get_entity('Class2').get_method('Method3')._params
     assert 'Param2' in load_dia.get_entity('Class2').get_method('Method3')._params
     assert 'Param3' in load_dia.get_entity('Class2').get_method('Method3')._params
+
+def test_all_together():
+    dia = Diagram()
+    dia.add_entity('Class1')
+    dia.add_entity('Class2')
+    dia.add_relation('Class1', 'Class2', 'composition')
+    dia.add_relation('Class2', 'Class1', 'inheritance')
+    dia.get_entity('Class1').add_field('Field1')
+    dia.get_entity('Class1').add_field('Field2')
+    dia.get_entity('Class2').add_field('Field1')
+    dia.get_entity('Class1').add_method_and_params('Method1')
+    dia.get_entity('Class2').add_method_and_params('Method1', 'Param1')
+    dia.get_entity('Class2').add_method_and_params('Method2')
+    dia.get_entity('Class2').add_method_and_params('Method3', 'Param1', 'Param2', 'Param3')
+    serialize(dia, path)
+
+    load_dia = Diagram()
+    load_dia.add_entity('Entity1')
+    load_dia.add_entity('Entity2')
+    load_dia.add_relation('Entity1', 'Entity2', 'composition')
+    load_dia.add_relation('Entity2', 'Entity1', 'realization')
+    load_dia.get_entity('Entity1').add_field('Field1')
+    load_dia.get_entity('Entity2').add_field('Field1')
+    load_dia.get_entity('Entity2').add_field('Field2')
+    load_dia.get_entity('Entity1').add_method_and_params('Method1')
+    load_dia.get_entity('Entity1').add_method_and_params('Method2')
+    load_dia.get_entity('Entity2').add_method_and_params('Method1', 'Param1')
+    deserialize(load_dia, path)
+    assert not load_dia.has_entity('Entity1')
+    assert not load_dia.has_entity('Entity2')
+
+    assert load_dia.has_entity('Class1')
+    assert load_dia.has_entity('Class2')
+    assert Relation('composition', Entity('Class1'), Entity('Class2')) in load_dia._relations
+    assert Relation('inheritance', Entity('Class2'), Entity('Class1')) in load_dia._relations
+    assert 'Field1' in load_dia.get_entity('Class1')._fields
+    assert 'Field2' in load_dia.get_entity('Class1')._fields
+    assert 'Field1' in load_dia.get_entity('Class2')._fields
+    assert load_dia.get_entity('Class1').get_method('Method1')
+    assert load_dia.get_entity('Class2').get_method('Method1')
+    assert 'Param1' in load_dia.get_entity('Class2').get_method('Method1')._params
+    assert load_dia.get_entity('Class2').get_method('Method2')
+    assert load_dia.get_entity('Class2').get_method('Method3')
+    assert 'Param1' in load_dia.get_entity('Class2').get_method('Method3')._params
+    assert 'Param2' in load_dia.get_entity('Class2').get_method('Method3')._params
+    assert 'Param3' in load_dia.get_entity('Class2').get_method('Method3')._params
