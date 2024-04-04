59f292d516b877ba81861520ad66965cb2143205
pmccullough2204 <mcculloughpatr@gmail.com>
2024-04-04 14:33:37 -0400

    actions test

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     license = "MIT", 
     author="Ganga Acharya, Marshall Feng, Peter Freedman, Adam Glick-Lynch, Tim Moser",
     author_email='grachary@millersville.edu, mdfeng@millersville.edu, pwfreedm@millersville.edu, ahglickl@millersville.edu, timbmoser@gmail.com',
-    url="https://github.com/mucsci-students/2024sp-420-CWorld.git",
+    url="https://github.com/mucsci-students/2024sp-420-PythonA.git",
     packages=find_packages(where='src'),
     include_package_data=True,
     package_dir={'': 'src'},
