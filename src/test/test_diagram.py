5c5672f1d83798beedb81a7da83fa14cc7390b43
katied0921 <kaitlyn.dowlin0921@gmail.com>
2024-04-10 13:28:37 -0400

    Uncommented out tests for list methods

--- src/test/test_diagram.py
+++ src/test/test_diagram.py
@@ -130,25 +130,25 @@ def test_dia_rename_multiple_entities():
     assert dia.has_entity("ent15")
     assert dia.has_entity("ent16")
 
-# def test_dia_list_entity_details_success():
-#     dia = Diagram()
-#     result = "y"
-#     assert dia.list_entity_details("ent1") == result
-#
-# def test_dia_list_entity_details_error():
-#     dia = Diagram()
-#     with pytest.raises(CustomExceptions.EntityNotFoundError):
-#         dia.list_entity_details("entity")
-#
-# def test_dia_list_everything():
-#     dia = Diagram()
-#     result = "y"
-#     assert dia.list_everything() == result
-#
-# def test_dia_list_entities():
-#     dia = Diagram()
-#     result = "class, ent1, ent2, ent3, ent4, ent5, ent6, ent7, ent8, ent9, ent10, ent11, ent12, ent13, ent14, ent15, ent16"
-#     assert dia.list_entities() == result
+def test_dia_list_entity_details_success():
+    dia = Diagram()
+    result = "y"
+    assert dia.list_entity_details("ent1") == result
+
+def test_dia_list_entity_details_error():
+    dia = Diagram()
+    with pytest.raises(CustomExceptions.EntityNotFoundError):
+        dia.list_entity_details("entity")
+
+def test_dia_list_everything():
+    dia = Diagram()
+    result = "y"
+    assert dia.list_everything() == result
+
+def test_dia_list_entities():
+    dia = Diagram()
+    result = "class, ent1, ent2, ent3, ent4, ent5, ent6, ent7, ent8, ent9, ent10, ent11, ent12, ent13, ent14, ent15, ent16"
+    assert dia.list_entities() == result
 
 def test_dia_add_relation_success():
     dia = Diagram()
