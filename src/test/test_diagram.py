e9d6b6fa6b2e9fb5d8287add3cc85bda4f2b4916
katied0921 <kaitlyn.dowlin0921@gmail.com>
2024-04-10 13:10:10 -0400

    Added error tests for add_relation()

--- src/test/test_diagram.py
+++ src/test/test_diagram.py
@@ -130,33 +130,55 @@ def test_dia_rename_multiple_entities():
     assert dia.has_entity("ent15")
     assert dia.has_entity("ent16")
 
-def test_dia_list_entity_details_success():
-    dia = Diagram()
-    result = "y"
-    assert dia.list_entity_details("ent1") == result
+# def test_dia_list_entity_details_success():
+#     dia = Diagram()
+#     result = "y"
+#     assert dia.list_entity_details("ent1") == result
+#
+# def test_dia_list_entity_details_error():
+#     dia = Diagram()
+#     with pytest.raises(CustomExceptions.EntityNotFoundError):
+#         dia.list_entity_details("entity")
+#
+# def test_dia_list_everything():
+#     dia = Diagram()
+#     result = "y"
+#     assert dia.list_everything() == result
+#
+# def test_dia_list_entities():
+#     dia = Diagram()
+#     result = "class, ent1, ent2, ent3, ent4, ent5, ent6, ent7, ent8, ent9, ent10, ent11, ent12, ent13, ent14, ent15, ent16"
+#     assert dia.list_entities() == result
 
-def test_dia_list_entity_details_error():
-    dia = Diagram()
-    with pytest.raises(CustomExceptions.EntityNotFoundError):
-        dia.list_entity_details("entity")
-
-def test_dia_list_everything():
-    dia = Diagram()
-    result = "y"
-    assert dia.list_everything() == result
-
-def test_dia_list_entities():
-    dia = Diagram()
-    result = "class, ent1, ent2, ent3, ent4, ent5, ent6, ent7, ent8, ent9, ent10, ent11, ent12, ent13, ent14, ent15, ent16"
-    assert dia.list_entities() == result
-
-def test_dia_add_relation():
+def test_dia_add_relation_success():
     dia = Diagram()
     dia.add_entity("ent17")
     dia.add_entity("ent18")
     assert len(dia._relations) == 0
     dia.add_relation("ent17", "ent18", "aggregation")
     assert len(dia._relations) == 1
+
+def test_dia_add_relation_source_doesnt_exist():
+    dia = Diagram()
+    with pytest.raises(CustomExceptions.EntityNotFoundError):
+        dia.add_relation("entity", "ent18", "aggregation")
+
+def test_dia_add_relation_destination_doesnt_exist():
+    dia = Diagram()
+    with pytest.raises(CustomExceptions.EntityNotFoundError):
+        dia.add_relation("ent17", "entity", "aggregation")
+
+def test_dia_add_relation_that_already_exists():
+    dia = Diagram()
+    with pytest.raises(CustomExceptions.RelationExistsError):
+        dia.add_relation("ent17", "ent18", "aggregation")
+
+def test_dia_add_relation_invalid_type():
+    dia = Diagram()
+    dia.add_entity("ent19")
+    dia.add_entity("ent20")
+    with pytest.raises(CustomExceptions.InvalidRelationTypeError):
+        dia.add_relation("ent19", "ent20", "relationship")
 
 def test_dia_delete_relation():
     dia = Diagram()
