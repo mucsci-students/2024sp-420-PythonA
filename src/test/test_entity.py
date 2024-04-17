2972f1579e146420aab7255758356389a93f2692
katied0921 <kaitlyn.dowlin0921@gmail.com>
2024-04-17 18:48:59 -0400

    Trying to improve code coverage and fix failing tests

--- src/test/test_entity.py
+++ src/test/test_entity.py
@@ -54,99 +54,100 @@ def test_add_field_invalid_type():
     with pytest.raises(CustomExceptions.FieldtypeNotFoundError):
         ent1.add_field("field3", "type")
 
-# def test_add_multiple_fields():
-#     ent1 = Entity("entity1")
-#     ent1.add_field("field1", "int")
-#     ent1.add_field("field2", "string")
-#     ent1.add_field("field3", "bool")
-#     assert ("field1", "int") in ent1._fields
-#     assert ("field2", "string") in ent1._fields
-#     assert ("field3", "bool") in ent1._fields
-#     assert ("field4", "int") not in ent1._fields
+def test_add_multiple_fields():
+    ent1 = Entity("entity1")
+    ent1.add_field("field4", "int")
+    ent1.add_field("field5", "string")
+    ent1.add_field("field6", "bool")
+    assert ("field4", "int") in ent1._fields
+    assert ("field5", "string") in ent1._fields
+    assert ("field6", "bool") in ent1._fields
+    assert ("field7", "int") not in ent1._fields
 
 def test_delete_field_success():
     ent1 = Entity("entity1")
-    assert ("field1", "int") not in ent1._fields
-    ent1.add_field("field1", "int")
-    assert ("field1", "int") in ent1._fields
-    ent1.delete_field("field1")
-    assert ("field1", "int") not in ent1._fields
+    assert ("field7", "int") not in ent1._fields
+    ent1.add_field("field7", "int")
+    assert ("field7", "int") in ent1._fields
+    ent1.delete_field("field7")
+    assert ("field7", "int") not in ent1._fields
 
 def test_delete_field_doesnt_exist():
     ent1 = Entity("entity1")
-    assert ("field", "bool") not in ent1._fields
+    assert ("field7", "bool") not in ent1._fields
     with pytest.raises(CustomExceptions.FieldNotFoundError):
-        ent1.delete_field("field1")
+        ent1.delete_field("field7")
 
-# def test_delete_multiple_fields():
-#     ent1 = Entity("entity1")
-#     ent1.add_field("field1", "int")
-#     ent1.add_field("field2", "string")
-#     ent1.add_field("field3", "bool")
-#     ent1.add_field("field4", "float")
-#     assert ("field1", "int") in ent1._fields
-#     assert ("field2", "string") in ent1._fields
-#     assert ("field3", "bool") in ent1._fields
-#     assert ("field4", "float") in ent1._fields
-#     ent1.delete_field("field1")
-#     ent1.delete_field("field2")
-#     ent1.delete_field("field3")
-#     assert ("field1") not in ent1._fields
-#     assert ("field2") not in ent1._fields
-#     assert ("field3") not in ent1._fields
-#     assert ("field4", "float") in ent1._fields
+def test_delete_multiple_fields():
+    ent1 = Entity("entity1")
+    ent1.add_field("field7", "int")
+    ent1.add_field("field8", "string")
+    ent1.add_field("field9", "bool")
+    ent1.add_field("field10", "float")
+    assert ("field7", "int") in ent1._fields
+    assert ("field8", "string") in ent1._fields
+    assert ("field9", "bool") in ent1._fields
+    assert ("field10", "float") in ent1._fields
+    ent1.delete_field("field7")
+    ent1.delete_field("field8")
+    ent1.delete_field("field9")
+    assert ("field7") not in ent1._fields
+    assert ("field8") not in ent1._fields
+    assert ("field9") not in ent1._fields
+    assert ("field10", "float") in ent1._fields
 
 def test_rename_field():
     ent1 = Entity("entity1")
-    assert ("field1","int") not in ent1._fields
-    ent1.add_field("field1", "int")
-    assert ("field1", "int") in ent1._fields
-    assert ("field2", "int") not in ent1._fields
-    ent1.rename_field("field1", "int","field2","int")
-    assert ("field1","int") not in ent1._fields
-    assert ("field2","int") in ent1._fields
+    assert ("field11","int") not in ent1._fields
+    ent1.add_field("field11", "int")
+    assert ("field11", "int") in ent1._fields
+    assert ("field12", "int") not in ent1._fields
+    ent1.rename_field("field11", "int","field12","int")
+    assert ("field11","int") not in ent1._fields
+    assert ("field12","int") in ent1._fields
 
 def test_rename_field_old_name_doesnt_exist():
     ent1 = Entity("entity1")
-    assert ("field3","int") not in ent1._fields
+    assert ("field13","int") not in ent1._fields
     with pytest.raises(CustomExceptions.FieldNotFoundError):
-        ent1.rename_field("field3", "int","field2","int")
+        ent1.rename_field("field13", "int","field2","int")
 
-def test_rename_field_old_name_doesnt_exist():
+def test_rename_field_new_name_already_exists():
     ent1 = Entity("entity1")
-    assert ("field1","string") not in ent1._fields
-    ent1.add_field("field1", "string")
-    assert ("field2", "string") not in ent1._fields
-    ent1.add_field("field2", "string")
+    assert ("field14","string") not in ent1._fields
+    ent1.add_field("field14", "string")
+    assert ("field15", "string") not in ent1._fields
+    ent1.add_field("field15", "string")
     with pytest.raises(CustomExceptions.FieldNotFoundError):
-        ent1.rename_field("field1", "string","field2","string")
+        ent1.rename_field("field14", "string","field15","string")
 
-# def test_rename_multiple_fields():
-#     ent1 = Entity("entity1")
-#     ent1.add_field("field1", "int")
-#     ent1.add_field("field2", "int")
-#     assert ("field1","int") in ent1._fields
-#     assert ("field2","int") in ent1._fields
-#     assert ("field3","int") not in ent1._fields
-#     assert ("field4","int") not in ent1._fields
-#     ent1.rename_field("field1", "int", "field3", "int")
-#     ent1.rename_field("field2", "int","field4", "int")
-#     assert ("field1", "int") not in ent1._fields
-#     assert ("field2", "int") not in ent1._fields
-#     assert ("field3", "int") in ent1._fields
-#     assert ("field4", "int") in ent1._fields
+def test_rename_multiple_fields():
+    ent1 = Entity("entity1")
+    ent1.add_field("field16", "int")
+    ent1.add_field("field17", "int")
+    assert ("field16","int") in ent1._fields
+    assert ("field17","int") in ent1._fields
+    assert ("field18","int") not in ent1._fields
+    assert ("field19","int") not in ent1._fields
+    ent1.rename_field("field16", "int", "field18", "int")
+    ent1.rename_field("field17", "int","field19", "int")
+    assert ("field16", "int") not in ent1._fields
+    assert ("field17", "int") not in ent1._fields
+    assert ("field18", "int") in ent1._fields
+    assert ("field19", "int") in ent1._fields
 
 def test_get_method_success():
     ent1 = Entity("entity1")
+    ent2 = Entity("entity2")
     assert not any(um._name == "method" and um._return_type == "string" for um in ent1._methods)
     ent1.add_method("method", "string")
-    assert ent1.get_method("method") == "method"
+    assert ent1.get_method("method") == ent2.add_method("method")
 
 def test_get_method_doesnt_exist():
     ent1 = Entity("entity1")
-    assert not any(um._name == "method" and um._return_type == "float" for um in ent1._methods)
+    assert not any(um._name == "method2" and um._return_type == "float" for um in ent1._methods)
     with pytest.raises(CustomExceptions.MethodNotFoundError):
-        assert ent1.get_method("method")
+        assert ent1.get_method("method2")
 
 def test_add_method_success():
     ent1 = Entity("entity1")
@@ -166,32 +167,32 @@ def test_add_method_invalid_return_type():
     with pytest.raises(CustomExceptions.MethodExistsError):
         ent1.add_method("method3", "type")
 
-# def test_add_mutliple_methods():
-#     ent1 = Entity("entity1")
-#     assert not any(um._name == "method1" and um._return_type == "void" for um in ent1._methods)
-#     assert not any(um._name == "method2" and um._return_type == "void" for um in ent1._methods)
-#     assert not any(um._name == "method3" and um._return_type == "void" for um in ent1._methods)
-#     assert not any(um._name == "method4" and um._return_type == "void" for um in ent1._methods)
-#     ent1.add_method("method1","void")
-#     ent1.add_method("method2", "void")
-#     ent1.add_method("method3","void")
-#     assert any(um._name == "method1" and um._return_type == "void" for um in ent1._methods)
-#     assert any(um._name == "method2" and um._return_type == "void" for um in ent1._methods)
-#     assert any(um._name == "method3" and um._return_type == "void" for um in ent1._methods)
-#     assert not any(um._name == "method4" and um._return_type == "void" for um in ent1._methods)
+def test_add_mutliple_methods():
+    ent1 = Entity("entity1")
+    assert not any(um._name == "method1" and um._return_type == "void" for um in ent1._methods)
+    assert not any(um._name == "method2" and um._return_type == "void" for um in ent1._methods)
+    assert not any(um._name == "method3" and um._return_type == "void" for um in ent1._methods)
+    assert not any(um._name == "method4" and um._return_type == "void" for um in ent1._methods)
+    ent1.add_method("method1","void")
+    ent1.add_method("method2", "void")
+    ent1.add_method("method3","void")
+    assert any(um._name == "method1" and um._return_type == "void" for um in ent1._methods)
+    assert any(um._name == "method2" and um._return_type == "void" for um in ent1._methods)
+    assert any(um._name == "method3" and um._return_type == "void" for um in ent1._methods)
+    assert not any(um._name == "method4" and um._return_type == "void" for um in ent1._methods)
 
 def test_delete_method_success():
     ent1 = Entity("entity1")
     ent1.add_method("method5", "bool")
     assert any(um._name == "method5" and um._return_type == "bool" for um in ent1._methods)
-    ent1.delete_method("method5", "bool")
+    ent1.delete_method("method5")
     assert not any(um._name == "method5" and um._return_type == "bool" for um in ent1._methods)
 
 def test_delete_method_doesnt_exist():
     ent1 = Entity("entity1")
     assert not any(um._name == "method6" and um._return_type == "void" for um in ent1._methods)
     with pytest.raises(CustomExceptions.MethodNotFoundError):
-        ent1.delete_method("method6", "void")
+        ent1.delete_method("method6")
 
 def test_rename_method_success():
     ent1 = Entity("entity1")
@@ -245,7 +246,6 @@ def test_add_multiple_param():
     method.add_parameters("param2")
     method.add_parameters("param3")
     method.add_parameters("param4")
-    assert ("param1") in method._params
     assert ("param2") in method._params
     assert ("param3") in method._params
     assert ("param4") in method._params
