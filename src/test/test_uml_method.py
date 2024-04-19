dcdbf2b7f0144aaffd3bc5cbeb56bcaab6c321b4
katied0921 <kaitlyn.dowlin0921@gmail.com>
2024-04-18 23:33:12 -0400

    Fixing code coverage in entity.py

--- src/test/test_uml_method.py
+++ src/test/test_uml_method.py
@@ -76,6 +76,13 @@ def test_change_one_param():
     assert "prm1" not in md1._params
     assert "prm2" in md1._params
 
+def test_change_duplicate_param():
+    md4 = UML_Method("md4", "bool")
+    md4.add_parameters("prm1")
+    md4.add_parameters("prm2")
+    with pytest.raises(CustomExceptions.ParameterExistsError):
+        md4.change_parameters("prm1", "prm2")
+
 def test_change_multiple_params():
     md1 = UML_Method("md1", "string")
     md1.add_parameters("prm1")
