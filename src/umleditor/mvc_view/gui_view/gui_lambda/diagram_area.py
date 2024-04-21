94f580bc2199f03e935745769ac204fe2dd2af8a
pmccullough2204 <mcculloughpatr@gmail.com>
2024-04-21 16:53:33 -0400

    Lines are pretty now!

--- src/umleditor/mvc_view/gui_view/gui_lambda/diagram_area.py
+++ src/umleditor/mvc_view/gui_view/gui_lambda/diagram_area.py
@@ -1,8 +1,10 @@
+import math
+
 from PyQt6.QtCore import pyqtSignal, Qt, QPoint, QUrl
 from PyQt6.QtWidgets import (QDialog, QMainWindow, QWidget, QVBoxLayout, QPushButton, QApplication, QGridLayout,
                              QMessageBox, QHBoxLayout, QRadioButton, QDialogButtonBox, QListWidget, QLabel, QFrame,
                              QFileDialog)
-from PyQt6.QtGui import QAction, QPainter, QPen, QColor,QDesktopServices, QPixmap
+from PyQt6.QtGui import QAction, QPainter, QPen, QColor, QDesktopServices, QPixmap, QPolygon
 from umleditor.mvc_view.gui_view.gui_lambda.class_card_revamp import ClassCard
 
 class DiagramArea(QWidget):
@@ -44,20 +46,48 @@ class DiagramArea(QWidget):
         self.classCards.clear()  # Clear the tracking dictionary
         self.relationships.clear()  # Clear any stored relationships
         self.update()  # Redraw the area
-                
+
     def paintEvent(self, event):
         super().paintEvent(event)
         painter = QPainter(self)
-        # Set the pen with the correct QColor usage
-        painter.setPen(QPen(QColor('black'), 2)) 
-        for src, dest in self.relationships:
+        painter.setRenderHint(QPainter.RenderHint.Antialiasing)  # For smoother lines
+
+        for src, dest, rel_type in self.relationships:
             if src in self.classCards and dest in self.classCards:
                 src_pos = self.classCards[src].centerPos()
                 dest_pos = self.classCards[dest].centerPos()
+
+                # Set styles based on relationship type
+                if rel_type == 'realization':
+                    painter.setPen(QPen(QColor('black'), 2, Qt.PenStyle.SolidLine))
+                elif rel_type == 'aggregation':
+                    painter.setPen(QPen(QColor('blue'), 2, Qt.PenStyle.DashLine))
+                    self.drawArrow(painter, src_pos, dest_pos, QColor('blue'))
+                elif rel_type == 'composition':
+                    painter.setPen(QPen(QColor('green'), 3, Qt.PenStyle.DashDotLine))
+                    self.drawArrow(painter, src_pos, dest_pos, QColor('green'))
+                elif rel_type == 'inheritance':
+                    painter.setPen(QPen(QColor('red'), 2, Qt.PenStyle.DotLine))
+                    self.drawArrow(painter, src_pos, dest_pos, QColor('red'))
+
                 painter.drawLine(src_pos, dest_pos)
 
-    def addRelationship(self, src_class_name, dest_class_name):
-        self.relationships.append((src_class_name, dest_class_name))
+    def drawArrow(self, painter, src_pos, dest_pos, color):
+        # Arrowhead drawing
+        arrow_size = 10
+        painter.setBrush(color)
+
+        angle = math.atan2(-(dest_pos.y() - src_pos.y()), dest_pos.x() - src_pos.x())
+        arrow_p1 = dest_pos + QPoint(math.sin(angle - math.pi / 3) * arrow_size,
+                                     math.cos(angle - math.pi / 3) * arrow_size)
+        arrow_p2 = dest_pos + QPoint(math.sin(angle - math.pi + math.pi / 3) * arrow_size,
+                                     math.cos(angle - math.pi + math.pi / 3) * arrow_size)
+
+        arrow_head = QPolygon([dest_pos, arrow_p1, arrow_p2])
+        painter.drawPolygon(arrow_head)
+
+    def addRelationship(self, src_class_name, dest_class_name, rel_type):
+        self.relationships.append((src_class_name, dest_class_name, rel_type))
         self.update()  # Request a repaint to draw the new line
         
     def removeRelationship(self, src_class_name, dest_class_name):
