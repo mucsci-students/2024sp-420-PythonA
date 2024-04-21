242916a50089ef89ca6281e4296d09308dd5c8a5
pmccullough2204 <mcculloughpatr@gmail.com>
2024-04-21 19:22:05 -0400

    We are in hell

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
-import sys, os
+import sys,os
 from PyQt6.QtWidgets import QApplication, QDialog
 
 from umleditor.mvc_controller.cli_controller import CLI_Controller
 from umleditor.mvc_view.gui_view.gui_lambda.GUIV2 import GUIV2
+from umleditor.mvc_view.gui_view.gui_lambda.GUI3 import GUI3
+from umleditor.mvc_view.gui_view.gui_cworld.view_GUI import ViewGUI
+from umleditor.mvc_controller.gui_controller import ControllerGUI
+from umleditor.mvc_controller.GUIV2_controller import ControllerGUI as ControllerGUIV2
+from umleditor.mvc_view.gui_view.gui_lambda.dialog_boxes.versionDialog import VersionSelectionDialog
 import sys
 
 
 def main():
-    # decides which main to run
+    #decides which main to run
     if len(sys.argv) >= 2 and sys.argv[1] == 'cli':
         mainCLI()
     elif len(sys.argv) >= 2 and sys.argv[1] == 'debug':
@@ -15,20 +20,16 @@ def main():
     else:
         mainGUI()
 
-
 def blockGuiTerminal():
     # will not print anything to the terminal
     sys.stdout = open(os.devnull, 'w')
-
-
 def debug_main():
-    # CLI main without some error catching
+    #CLI main without some error catching
     app = CLI_Controller()
     app.run()
 
-
 def mainCLI():
-    # main CLI execution
+    #main CLI execution
     try:
         app = CLI_Controller()
         app.run()
@@ -47,12 +48,35 @@ def mainGUI():
     blockGuiTerminal()
     app = QApplication(sys.argv)
 
-    mainWindow = GUIV2()
+    try:
+        dialog = VersionSelectionDialog()
+        result = dialog.exec()
 
-    # Ensure ControllerGUI is correctly initialized
-    mainWindow.show()
-    sys.exit(app.exec())
+        if result == QDialog.DialogCode.Accepted:
+            if dialog.selected_version == "1":
+                mainWindow = ViewGUI()
+                controller = ControllerGUI(mainWindow)
+            elif dialog.selected_version == "2":
+                mainWindow = GUIV2()
+                controller = ControllerGUIV2(mainWindow)
+            elif dialog.selected_version == "3":
+                mainWindow = GUI3()
+            else:
+                print("Invalid selection, defaulting to Gui V2.")
+                mainWindow = GUIV2()
 
+              # Ensure ControllerGUI is correctly initialized
+            mainWindow.show()
+            sys.exit(app.exec())
+        else:
+            print("No selection made, exiting.")
+            sys.exit(0)
+    except KeyboardInterrupt:
+        # This handles ctrl+C gracefully
+        print("\nApplication closed.")
+    except Exception as e:
+        # Generic exception handling
+        print(f"Unexpected error occurred: {e}")
 
 if __name__ == '__main__':
     main()
