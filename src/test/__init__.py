abfe407335dca2a5fd98005e5153031de25fa8c3
katied0921 <kaitlyn.dowlin0921@gmail.com>
2024-04-21 23:15:40 -0400

    Trying to fix import warning that's preventing code coverage report from being generated

---
+++ src/test/__init__.py
@@ -0,0 +1,9 @@
+import sys
+sys.path.append('src/test')
+
+from .test_diagram import *
+from .test_entity import *
+from .test_imports import *
+from .test_relation import *
+from .test_serializer import *
+from .test_uml_method import *
