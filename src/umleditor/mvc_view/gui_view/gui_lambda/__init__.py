292aafece7e09aac32c62b25bae83cbdd693b2ce
katied0921 <kaitlyn.dowlin0921@gmail.com>
2024-04-25 21:53:00 -0400

    Commented out dialog_boxes import in __init__py in gui_lambda

--- src/umleditor/mvc_view/gui_view/gui_lambda/__init__.py
+++ src/umleditor/mvc_view/gui_view/gui_lambda/__init__.py
@@ -4,4 +4,4 @@ sys.path.append('src/umleditor/gui_view/gui_lambda')
 # from .GUI3 import *
 # from .GUIV2_class_card import *
 # from .GUIV2 import *
-from .dialog_boxes import *
+# from .dialog_boxes import *
