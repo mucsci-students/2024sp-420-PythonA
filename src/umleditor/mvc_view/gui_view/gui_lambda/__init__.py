bd3c664ce9951b8c9ac9611f21da906209285e3a
katied0921 <kaitlyn.dowlin0921@gmail.com>
2024-04-25 21:39:40 -0400

    Commented out GUI3 import in __init__py in gui_lambda

--- src/umleditor/mvc_view/gui_view/gui_lambda/__init__.py
+++ src/umleditor/mvc_view/gui_view/gui_lambda/__init__.py
@@ -1,7 +1,7 @@
 import sys
 sys.path.append('src/umleditor/gui_view/gui_lambda')
 
-from .GUI3 import *
+# from .GUI3 import *
 from .GUIV2_class_card import *
 from .GUIV2 import *
 from .dialog_boxes import *
